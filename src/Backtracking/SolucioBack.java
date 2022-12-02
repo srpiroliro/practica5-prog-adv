6d0f5f6faff930c669d24e1bb47b01aad2657be0
srpiroliro <srpiroliro21@gmail.com>
2022-12-02 09:48:37 +0100

    created files + added todo

---
+++ src/Backtracking/SolucioBack.java
@@ -0,0 +1,20 @@
+/*
+1.- Per què l’esquema de backtracking és aplicable per a resoldre aquest enunciat. 
+2.- Indica quina pregunta et fas en cada nivell de l’arbre. Quines són les possibles respostes d’aquesta pregunta (domini). 
+3.- Quin és el criteri per determinar si una decisió és o no acceptable. 
+4.- Quin és el criteri per determinar si un conjunt de decisions és o no completable. 
+5.- Quin és el criteri per determinar si un conjunt de decisions són o no solució. 
+6.- Dibuixeu l’espai de cerca del problema, és a dir l’arbre que ha de recórrer la tècnica del backtracking, indica quina és l’alçada i l’amplada i si són valors exactes o valors màxims. Usaràs marcatge?. 
+7.- Quin és el criteri per determinar si una solució és o no millor a una altra ja trobada prèviament.
+*/
+
+package Backtracking;
+
+class SolucioBack {
+    // attrs:
+    // - magatzem amb tots els Producte que ha d'emmagatzemar l'empresa
+    // - magatzems per emmagatzemar diferentes solucions
+    // - atributs per controlar si es la millor solucio fins ara o no
+    // - matriu d'adjecencia que s'ha de construir a partir de incopatibilitats
+    //  dels productes. (consultara el backtracking si una decisio es acceptable)  
+}
---
+++ src/Producte/Producte.java
@@ -0,0 +1,9 @@
+package Producte;
+
+class Producte{
+
+    private int id=0;
+    private Producte reactius[];
+
+    // ?
+}
