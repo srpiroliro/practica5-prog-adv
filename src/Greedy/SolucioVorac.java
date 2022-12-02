c0fda6403f2b65c720af956eb32a54bf3449af51
srpiroliro <srpiroliro21@gmail.com>
2022-12-02 09:48:49 +0100

    created files + added todo

---
+++ src/Greedy/SolucioVorac.java
@@ -0,0 +1,15 @@
+package Greedy;
+
+/*
+1.- Perquè aquest problema pot resoldre’s aplicant la tècnica voraç? 
+2.- Quins són els candidats del problema? 
+3.- Quina funció de selecció aplicarà el vostre algorisme? Cal trobar la millor 
+4.- La vostra funció de selecció, garanteix trobar sempre la millor solució?  Per què? 
+*/
+
+public class SolucioVorac {
+    // magatzem amb tots els productes que s'han d'emmagatzemar
+    // magatzems per emmagatzemar la solucio vorac
+    // 
+    // ++ metodes i atributs
+}
