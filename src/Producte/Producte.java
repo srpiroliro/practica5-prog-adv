1e47634b13017e87e4abdb003222a926d2c5ddf9
srpiroliro <srpiroliro21@gmail.com>
2022-12-20 17:11:18 +0100

    anjskdja

--- src/Producte/Producte.java
+++ src/Producte/Producte.java
@@ -1,9 +1,20 @@
 package Producte;
+import java.util.HashSet;
 
 class Producte{
-
     private int id=0;
-    private Producte reactius[];
+    private HashSet<Producte> reactives=new HashSet<Producte>();
+    
+    public Producte(int id){this.id=id;}
+    public Producte(int id,HashSet<Producte> ps){
+        this.id=id;
+        reactives=ps;
+    }
 
-    // ?
+    public boolean add_reactive(Producte r){
+        if(r==null) return false; //||reactives.contains(r) necesary?
+        reactives.add(r);
+        return true;
+    }
+    // https://www.javatpoint.com/java-graph
 }
